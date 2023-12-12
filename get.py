ec1732db1004afa1f0b641d5e287c30bc2443b43
h3l2f <88704154+h3l2f@users.noreply.github.com>
2023-12-12 19:34:36 +0700

    Update get.py
    Allows you to customize whether to hide or show links in the get.py file

--- get.py
+++ get.py
@@ -4,7 +4,15 @@ with open("link.json","r") as f:
     d = json.load(f)
 l = list(d["link"].keys())
 
+#Config can viewer see the original code
+#y = show // n= No
+
+is_showing = "y"
+
 print(f'total: {len(l)}<br>')
 for i in range(0,len(l)):
-    print(f'{l[i]} : {d.get("link").get(l[i]).get("url")}<br>')
+    link = d.get("link").get(l[i]).get("url")
+    if is_showing == "n":
+        link = "Hidden as config!"
+    print(f'{l[i]} : {link}<br>')
     i += 1
